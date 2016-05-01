a0cd20f0698fcbbe3c9e9e0cb175d29c393a2f8a
catonrug <aigars.kadikis@gmail.com>
2016-05-01 20:20:35 +0300

    completed

---
+++ install.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#update all repositories and install latest updates
+apt-get update -y && apt-get upgrade
+
+#install nginx web server
+apt-get install nginx php5-fpm php5-cli php5-mcrypt php5-gd -y
+
+#move to the home directory
+cd
+
+#Download dokuwiki from official home page
+wget http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz -O dokuwiki.tgz
+
+#extract archive
+tar -xvf dokuwiki.tgz
+
+#set up nginx  
+cat > /etc/nginx/sites-available/yourdomain.com << EOF
+server {
+server_name yourdomain.com;
+listen 80;
+root /var/www/yourdomain.com/;
+access_log /var/log/nginx/yourdomain.com-access.log;
+error_log /var/log/nginx/yourdomain.com-error.log;
+
+index index.php index.html doku.php;
+location ~ /(data|conf|bin|inc)/ {
+      deny all;
+}
+location ~ /\.ht {
+      deny  all;
+}
+location ~ \.php {
+fastcgi_index index.php;
+fastcgi_split_path_info ^(.+\.php)(.*)\$;
+include /etc/nginx/fastcgi_params;
+fastcgi_pass unix:/var/run/php5-fpm.sock;
+fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+}
+}
+EOF
+
+#make symbolic limk
+ln -s /etc/nginx/sites-available/yourdomain.com /etc/nginx/sites-enabled/yourdomain.com
+
+#restart nginx server
+/etc/init.d/nginx restart
+
+#make direcotry
+mkdir -p /var/www/yourdomain.com
+
+#move to extracted content
+cd ~/dokuwiki-*
+
+#copy all content to your domain directory
+cp -a . /var/www/yourdomain.com
+
+#let nginx operate with this content
+chown -R www-data:www-data /var/www/yourdomain.com
